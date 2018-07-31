995ced6d20df27ba5397b917148bff4d523d54f9
Josh <jkopman@shutterstock.com>
2018-07-31 14:56:34 -0400

    initial

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.get('/', function(req, res) {
 
 app.listen(3000);
 
-const wss = new WebSocketServer({ app });
+var SocketServer = require('ws').Server,
+  wss = new SocketServer({port: 40510})
   wss.on('connection', function (ws) {
   	ws.on('message', function (message) {
     	console.log('received: %s', message)
