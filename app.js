35e2f2aa6d5c3a8513aeff2542e961b1c0f2c95e
Josh <jkopman@shutterstock.com>
2018-07-31 15:42:09 -0400

    running one lighthouse for all socket clients

--- app.js
+++ app.js
@@ -4,23 +4,19 @@ const SocketServer = require('ws').Server;
 var lighthouseLauncher = require('./lighthouseLauncher');
 var currentAverage = 0;
 var noOfRuns = 1;
+var currentScore = 0;
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
-
+var cxo
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const wss = new SocketServer({ server });
-  wss.on('connection', function (ws) {
-  	ws.on('message', function (message) {
-    	console.log('received: %s', message)
-  	})
-	setInterval(function(){
+setInterval(function(){
  	 new lighthouseLauncher().launchChromeAndRunLighthouse('https://www.shutterstock.com', {chromeFlags: ['--headless']}).then(results => {
      setTimeout(() => {
       if (results.categories.performance.score) {
-        const currentScore = results.categories.performance.score * 100;
+        currentScore = results.categories.performance.score * 100;
         if (currentScore >= 100) {
             console.info(`Performance score is 100.`);
             process.exit(0);
@@ -30,8 +26,6 @@ const wss = new SocketServer({ server });
         }
         currentAverage = (currentAverage + currentScore) / 2;
         noOfRuns++;
-        ws.send(JSON.stringify({score:currentScore,runs:noOfRuns}));
-
         //console.log("Average Performance Score is "+avg +" over "+ noOfRuns + " runs")
        // process.exit(1);
     }
@@ -39,5 +33,20 @@ const wss = new SocketServer({ server });
    // process.exit(1);
     }, 2000); 
 	}); 
-	},5000);
+},5000);
+
+const wss = new SocketServer({ server });
+
+  wss.on('connection', function (ws) {
+  	setInterval(function(){
+  		try{
+   			ws.send(JSON.stringify({score:currentScore,runs:noOfRuns}));
+   		}
+   		catch{
+   			console.log('connection closed');
+   		}
+	},2000);
+  	ws.on('message', function (message) {
+    	console.log('received: %s', message)
+  	})
 })
