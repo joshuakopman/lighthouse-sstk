edbf08ee0c9b2d73cefea87563de45441eb9ef11
Josh <jkopman@shutterstock.com>
2018-09-14 19:45:08 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,44 +1,19 @@
 var express = require('express');
-var path = require('path');
-var lighthouseLauncher = require('./lighthouseLauncher');
-
-var currentAverage = 0;
-var noOfRuns = 1;
-var currentScore = 0;
-
+var Runner = require('./Runner');
+const runner = new Runner();
 const SocketServer = require('ws').Server;
 const PORT = process.env.PORT || 3000;
 const server = express()
-  .use((req, res) => res.sendFile('index.html', { root: __dirname }))
+  .use('/logs', express.static('logs'))
+  .use('/assets', express.static('assets'))
+  .use('/',(req, res) => res.sendFile('index.html', { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-const wss = new SocketServer({ server });
-
-setInterval(function(){
-    new lighthouseLauncher().launchChromeAndRunLighthouse('https://www.shutterstock.com', {chromeFlags: ['--headless']}).then(results => {
-        setTimeout(() => {
-          if (results.categories.performance.score) {
-           currentScore = results.categories.performance.score * 100;
-            if (currentScore >= 100) {
-              console.info(`Performance score is 100.`);
-              process.exit(0);
-            }
-            if(currentAverage == 0){
-              currentAverage = currentScore;
-            }
-            currentAverage = (currentAverage + currentScore) / 2;
-            noOfRuns++;
-        }
-      }, 2000); 
-	}); 
-},5000);
-
-  wss.on('connection', function (ws) {
-  	setInterval(function(){
-  		try{
-   			ws.send(JSON.stringify({score:currentScore,runs:noOfRuns}));
-   		}
-   		catch(err){
-   			console.log('connection closed');
-   		}
-	},2000);
+const wss = new SocketServer({ server })
+  .on('error', function(e){ 
+    console.log('server socket error: '+e);
   })
+ .on('close', function(e){ 
+    console.log('server socket closed: ' +e);
+  });
+
+runner.startAudit(wss);
--- lighthouseLauncher.js
+++ lighthouseLauncher.js
@@ -1,17 +1,22 @@
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
-var lighthouseLauncher = function(){
+
+var LighthouseLauncher = function(){
 
   return {
        launchChromeAndRunLighthouse: function(url, opts, config = null) {
-        return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+        return chromeLauncher.launch().then(chrome => {
           opts.port = chrome.port;
           return lighthouse(url, opts, config).then(results => {
             return chrome.kill().then(() => results.lhr)
+          }).catch(error => {
+            console.log(error);
           });
-        });
+        }).catch(error => {
+          console.log(error);
+       });
       }
 }
 
 }
-module.exports = lighthouseLauncher
+module.exports = LighthouseLauncher
