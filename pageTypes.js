4914157b110cf5fe6f15bd44874469ec3a34b71a
Josh <jkopman@shutterstock.com>
2018-12-11 15:07:20 -0500

    adding page type constants

---
+++ pageTypes.js
@@ -0,0 +1,27 @@
+module.exports = pageTypes = [
+    {
+      "name": "lohp",
+      "title": "Logged Out Homepage",
+      "url":'https://www.shutterstock.com'
+    },
+    {
+      "name": "adp",
+      "title": "Asset Detail Page",
+      "url":"https://www.shutterstock.com/image-photo/happy-owner-carry-new-labrador-retriever-780540799"
+    },
+    {
+      "name": "cmsStudioExplore",
+      "title": "Studio Contentful Explore Page",
+      "url":"https://www.shutterstock.com/explore/royalty-free-images"
+    },
+     {
+      "name": "cmsStudioLOHP",
+      "title": "Studio Contentful LOHP Page",
+      "url":"https://www.shutterstock.com/discover/test-home-page"
+    },
+    {
+      "name": "search",
+      "title": "Search Results Page",
+      "url":"https://www.shutterstock.com/search/dog"
+    }
+  ];
