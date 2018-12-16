772c55d6293d19009ce479682a254f353e47ecb5
Joshua Kopman <joshuakopman@MacBook-Pro.local>
2018-12-15 23:11:55 -0500

    moving socket logic to container

--- src/components/LighthouseContainer.js
+++ src/components/LighthouseContainer.js
@@ -4,12 +4,16 @@ import TextScore from './TextScore.js';
 import Opportunity from './Opportunity.js';
 import Average from './Average.js';
 
+
 export default class LighthouseContainer extends React.Component {
-  constructor(props) {
+
+ constructor(props) {
     super(props);
     this.state = {
       pageTypes : [],
     };
+    this.host = location.origin.replace(/^http/, 'ws');
+    this.ws = new WebSocket(this.host)
   }
 
   componentDidMount() {
@@ -19,6 +23,34 @@ export default class LighthouseContainer extends React.Component {
         let pageTypes = data.pageTypes; 
         this.setState({ pageTypes : pageTypes }); 
       })
+
+    this.ws.onmessage = function (ev) { 
+      var payload = JSON.parse(ev.data);
+       for(var index in payload.globals.pageTypes){
+            var currentPage = payload.globals.pageTypes[index].name;
+            if(document.querySelector("#" + currentPage + "Score")){
+                document.querySelector("#" + currentPage + "Score").innerHTML = payload.globals.pages[currentPage].currentAverage.toFixed(2);
+            }
+            if(document.querySelector("#" + currentPage + "Runs")){
+                document.querySelector("#" + currentPage + "Runs").innerHTML = payload.globals.pages[currentPage].noOfRuns;
+            }
+            if(payload.globals.pages[currentPage].opportunitiesArray.length > 0){
+                document.querySelector("#" + currentPage + "Opportunities").innerHTML = formatOpsString(payload.globals.pages[currentPage].opportunitiesArray);
+            }
+
+            document.querySelectorAll("." + currentPage + "URL").forEach(el => {
+                el.href = payload.globals.pages[currentPage].url;
+                el.innerHTML = payload.globals.pages[currentPage].url;
+            });
+        }
+
+        showActiveTest(payload.globals.testRunningID);
+
+    }
+  }
+
+  componentWillUnmount() {
+    this.ws.close();
   }
 
   render() {
@@ -30,7 +62,7 @@ export default class LighthouseContainer extends React.Component {
               <h1>Lighthouse</h1>
               <img src="https://developers.google.com/web/progressive-web-apps/images/pwa-lighthouse.png" height="75px"/>
           </div>
-          <div className="desc">An app to run a Lighthouse audit on shutterstock.com every five minutes and aggregate the results.</div>
+          <div className="desc">An app to run a Lighthouse audit every five minutes and aggregate the results.</div>
           <div id="textScoresContainer" className="textScores">
             {pageTypes.map((pageType) => {
                return <TextScore {...pageType} />
@@ -54,4 +86,36 @@ export default class LighthouseContainer extends React.Component {
       </div>
   );
  }
+
+ formatOpsString(opportunities){
+        var opString = '';
+        for(var index in opportunities){   
+            if(opportunities[index] && opportunities[index].overallSavingsMs > 0){
+              opString += "<span style=\"font-weight:bold\">"+ opportunities[index].description + "</span> |  Count: <span style=\"color:red\">" + opportunities[index].count + "</span> |  Time Savings: <span style=\"color:red\">" + opportunities[index].overallSavingsMs.toFixed(2) + " ms</span><br></br>";
+            }
+        }
+        return opString;
+    }
+
+ showActiveTest(ID){
+         document.querySelectorAll('.textScores span').forEach(el => {
+            el.style.color = 'black';
+         });    
+
+         document.querySelectorAll('.score').forEach(el => {
+            el.style.color = '#f54336';
+         });    
+
+         document.querySelectorAll('.running').forEach(el => {
+            el.style.display = 'none';
+         });
+        
+
+        if(ID != '') {
+            document.querySelector("#"+ID).style.color = 'white';
+            document.querySelector("#"+ID+"Score").style.color = 'white';
+            document.querySelector("#"+ID+"Runs").style.color = 'white';
+            document.querySelector("#"+ID+"Running").style.display = 'inline';
+        }
+    }
 }
