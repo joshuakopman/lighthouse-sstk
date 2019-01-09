0112abb2c4a9b621a4d8ec8dc302df785afe9866
Josh <jkopman@shutterstock.com>
2019-01-09 10:07:52 -0500

    monitoring EIL instead of interactive

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 module.exports = PerformanceKey = "performance";
 module.exports = TimeToFirstByteKey = "time-to-first-byte";
 module.exports = FirstContentfulPaintKey = "first-contentful-paint";
-module.exports = InteractiveKey = "interactive";
+module.exports = EstimatedInputLatency = "estimated-input-latency";
 module.exports = ONE_DAY =  24 * 60 * 60 * 1000;
